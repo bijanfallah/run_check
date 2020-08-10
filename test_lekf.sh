244726dfc3c8d6b4d22a92bd3d38d536ad599745
fallah <bijan.fallah@met.fu-berlin.de>
2020-08-10 10:46:46 +0200

    updated the code

--- test_lekf.sh
+++ test_lekf.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -ex
-exp1="emis_lab21"
-exp2="emis_lab03"
-machine="bijanf@curta.zedat.fu-berlin.de:~/projects/"
-scp ${machine}${exp2}/S-VELD-BER/2018test/v3.0.002/output/LE_v3.0.002_conc-sfc_20180701_xb.nc .
-scp ${machine}${exp1}/S-VELD-BER/2018test/v3.0.002/output/LE_v3.0.002_conc-sfc_20180701_xf.nc .
-cdo timmean -sub LE_v3.0.002_conc-sfc_20180701_xf.nc LE_v3.0.002_conc-sfc_20180701_xb.nc test.nc
-ncview test.nc
+exp1="emis_lab34"
+exp2="emis_lab34"
+machine="bijanf@curta.zedat.fu-berlin.de:/scratch/bijanf/projects/"
+scp ${machine}${exp1}/S-VELD-BER/2018test/v3.0.002/output/LE_v3.0.002_conc-sfc_20180702_xb.nc ./1.nc
+scp ${machine}${exp2}/S-VELD-BER/2018test/v3.0.002/output/LE_v3.0.002_conc-sfc_20180702_xf.nc ./2.nc
+#scp ${machine}${exp1}/S-VELD-BER/2018test/v3.0.002/output/LE_v3.0.002_conc-3d_20180701_xf.nc ./1.nc
+#scp ${machine}${exp2}/S-VELD-BER/2018test/v3.0.002/output/LE_v3.0.002_conc-3d_20180701_xf.nc ./2.nc 
+
+cdo -sub -fldmean 2.nc -fldmean 1.nc test.nc
+#ncview test.nc
+#scp ${machine}emis_markus/S-VELD-BER/2018mark/v3.0.002/output/LE_v3.0.002_conc-sfc_20180701_xb.nc ./markus.nc
+#scp ${machine}emis_mark2/S-VELD-BER/2018mark/v3.0.002/output/LE_v3.0.002_conc-sfc_20180701_xb.nc ./ref.nc
+#cdo timmean -sub markus.nc ref.nc diff.nc 
+ncview test.nc 
